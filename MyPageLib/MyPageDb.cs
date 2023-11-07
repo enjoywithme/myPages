b2fed7e8b9bfaaa2c0b6c6f4729dff9a3f111dea
Duan Xin <xxinduan@outlook.com>
2023-11-07 11:14:33 +0800

    升级 movietorrents和mypageview到.net6

---
+++ MyPageLib/MyPageDb.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using MyPageLib.PoCo;
+using SqlSugar;
+
+namespace MyPageLib
+{
+    public class MyPageDb
+    {
+        public static MyPageDb Instance { get; } = new();
+
+        public string DataBaseName => Path.Combine(MyPageSettings.Instance.WorkingDirectory, "myPage.db");
+
+        public string ConnectionString => $"Data Source={DataBaseName};";
+
+        //用单例模式
+        private readonly SqlSugarScope _db;
+
+        public MyPageDb()
+        {
+            _db = new SqlSugarScope(new ConnectionConfig()
+            {
+                ConnectionString = ConnectionString,
+                DbType = SqlSugar.DbType.Sqlite,
+                IsAutoCloseConnection = true
+            }
+                );
+        }
+
+        public async void InsertUpdateDocument(PageDocumentPoCo documentPoCo)
+        {
+            try
+            {
+                var poCo = await _db.Queryable<PageDocumentPoCo>().FirstAsync(x => x.FilePath == documentPoCo.FilePath);
+                if (poCo != null)
+                {
+                    if (documentPoCo.Guid != poCo.Guid)
+                        documentPoCo.Guid = poCo.Guid;
+                    await _db.Updateable(documentPoCo).ExecuteCommandAsync();
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(documentPoCo.Guid))
+                        documentPoCo.Guid = Guid.NewGuid().ToString();
+                    await _db.Insertable(documentPoCo).ExecuteCommandAsync();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// 更新所有纪录的本地文件标志为FALSE
+        /// </summary>
+        public void UpdateLocalPresentFalse()
+        {
+            _db.Updateable<object>()
+                .AS("PG_DOCUMENT")
+                .SetColumns("LOCAL_PRESENT", 0)
+                .Where("LOCAL_PRESENT=1")
+                .ExecuteCommand();
+        }
+
+
+        public async void DeleteDocument(PageDocumentPoCo documentPoCo)
+        {
+
+            var list = new List<PageDocumentPoCo> { documentPoCo };
+            await _db.Deleteable<PageDocumentPoCo>().WhereColumns(list, it => new { it.FilePath }).ExecuteCommandAsync();
+        }
+
+        public PageDocumentPoCo FindFilePath(string filePath)
+        {
+            return _db.Queryable<PageDocumentPoCo>().First(it => it.FilePath == filePath);
+        }
+
+        public PageDocumentPoCo FindOriginUrl(string originUrl)
+        {
+            return _db.Queryable<PageDocumentPoCo>().First(it => it.OriginUrl == originUrl);
+        }
+
+        /// <summary>
+        /// 按照更改日期返回最后N条纪录
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public IList<PageDocumentPoCo> FindLastN(int n)
+        {
+            return _db.Queryable<PageDocumentPoCo>().OrderByDescending(co => co.DtModified).Take(n).ToList();
+        }
+
+        public IList<PageDocumentPoCo> FindLastDays(int n)
+        {
+            var dt = DateTime.Now.AddDays(-n);
+            return _db.Queryable<PageDocumentPoCo>().Where(co=>co.DtModified>=dt).OrderByDescending(co => co.DtModified).ToList();
+        }
+
+        /// <summary>
+        /// 删除一组文档
+        /// </summary>
+        /// <param name="documents"></param>
+        public bool BatchDelete(IList<PageDocumentPoCo> documents,out string message)
+        {
+            
+            try
+            {
+                _db.Deleteable<PageDocumentPoCo>(documents).ExecuteCommand(); //批量删除
+                foreach (var co in documents)
+                {
+                    if(File.Exists(co.FilePath))
+                        File.Delete(co.FilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return false;
+            }
+
+            message = $"成功删除{documents.Count}条纪录。";
+            return true;
+        }
+
+        private ConditionalCollections BuildConditionalCollections(string field, string[] values)
+        {
+            var list = new List<KeyValuePair<WhereType, ConditionalModel>>();
+
+            var first = true;
+            foreach (var split in values)
+            {
+                list.Add(new KeyValuePair<WhereType, ConditionalModel>(first ? WhereType.Or : WhereType.And,
+                    new ConditionalModel()
+                    {
+
+                        FieldName = field,
+                        ConditionalType = ConditionalType.InLike,
+                        FieldValue = split
+                    }));
+                first = false;
+            }
+
+            var c = new ConditionalCollections
+            {
+                ConditionalList = list
+            };
+            return c;
+
+        }
+
+        private ConditionalCollections BuildNotDeletedCondition()
+        {
+            var list = new List<KeyValuePair<WhereType, ConditionalModel>>();
+
+            list.Add(new KeyValuePair<WhereType, ConditionalModel>(WhereType.And,
+                new ConditionalModel
+                {
+
+                    FieldName = "LOCAL_PRESENT",
+                    ConditionalType = ConditionalType.Equal,
+                    FieldValue = "1"
+                }));
+            list.Add(new KeyValuePair<WhereType, ConditionalModel>(WhereType.And,
+                new ConditionalModel
+                {
+
+                    FieldName = "DELETED",
+                    ConditionalType = ConditionalType.Equal,
+                    FieldValue = "0"
+                }));
+
+            var c = new ConditionalCollections
+            {
+                ConditionalList = list
+            };
+            return c;
+
+        }
+
+
+        public IList<PageDocumentPoCo> FindFolderPath(string folderPath)
+        {
+            try
+            {
+                return _db.Queryable<PageDocumentPoCo>().Where(it => it.FolderPath == folderPath).ToList();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// https://www.donet5.com/home/Doc?typeId=2314
+        /// </summary>
+        /// <param name="searchString"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<IList<PageDocumentPoCo>> Search(string searchString, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(searchString) || searchString.Length < 2)
+                    return null;
+
+                var splits = searchString.Split(Array.Empty<char>(), StringSplitOptions.RemoveEmptyEntries);
+
+                var conModels = new List<IConditionalModel>
+                {
+                    BuildConditionalCollections("Title", splits),
+                    BuildConditionalCollections("FilePath", splits),
+                    BuildNotDeletedCondition()
+                };
+
+                return await _db.Queryable<PageDocumentPoCo>().Where(conModels).ToListAsync(cancellationToken);
+
+
+                //return await _db.Queryable<PageDocumentPoCo>().Where(it => 
+                //        splits.All(s=> it.Title.Contains(s)) 
+                //        || splits.All(s=>it.FilePath.Contains(s))
+                //        )
+                //    .ToListAsync(cancellationToken);
+
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ MyPageLib/PageIndexer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using MyPageLib.PoCo;
+
+namespace MyPageLib
+{
+    public class PageIndexer
+    {
+        public static PageIndexer Instance { get; } =new();
+
+        public event EventHandler IndexStopped;
+        public event EventHandler<string> IndexFileChanged; 
+
+        public bool IsRunning { get; private set; }
+        private bool _stopPending;
+
+        public void Start()
+        {
+            if(IsRunning) return;
+            _stopPending=false;
+            Task.Run(Processing);
+
+        }
+
+        public void Stop()
+        {
+            _stopPending=true;
+
+        }
+
+
+        private void Processing()
+        {
+            IsRunning = true;
+            try
+            {
+                //先更新所有纪录的Local present 为 false
+                MyPageDb.Instance.UpdateLocalPresentFalse();
+
+                foreach (var folder in MyPageSettings.Instance.ScanFolders)
+                {
+                    foreach (var file in Directory.EnumerateFiles(folder, "*.piz", SearchOption.AllDirectories))
+                    {
+                        Debug.WriteLine(file);
+                        IndexFileChanged?.Invoke(this, file);
+
+
+                        if (_stopPending) break;
+
+                        var poCo = new PageDocumentPoCo()
+                        {
+                            FilePath = file,
+                            Name = Path.GetFileNameWithoutExtension(file),
+                            LocalPresent = 1
+                        };
+                        poCo.CheckInfo();
+                        MyPageDb.Instance.InsertUpdateDocument(poCo);
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            }
+            
+
+            IsRunning = false;
+            IndexStopped?.Invoke(this,EventArgs.Empty);
+        }
+
+        
+
+    }
+}
